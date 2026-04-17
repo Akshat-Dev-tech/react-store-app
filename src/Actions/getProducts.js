4d096f11f70e267775d14678aac1b5a0c42eec3e
Akshat-Dev-tech <akiiwork1234@gmail.com>
2026-04-17 15:13:15 +0530

    concerted fn to async fn

--- src/Actions/getProducts.js
+++ src/Actions/getProducts.js
@@ -1,11 +1,27 @@
-export const getProducts = () =>{
-    return async (dispatch , state) =>{
-      console.log('getProducts action called with state:', state())
-      dispatch({type:'SET_LOADER',payload:true})
-      const response = await fetch('https://fakestoreapi.com/products')
-      const data = await response.json()
-      console.log(data)
-      dispatch({type:'ADD_PRODUCT',payload:data})
-      dispatch({type:'SET_LOADER',payload:false})
-    }
+// export const getProducts = () =>{
+//     return async (dispatch , state) =>{
+//       console.log('getProducts action called with state:', state())
+//       dispatch({type:'SET_LOADER',payload:true})
+//       const response = await fetch('https://fakestoreapi.com/products')
+//       const data = await response.json()
+//       console.log(data)
+//       dispatch({type:'ADD_PRODUCT',payload:data})
+//       dispatch({type:'SET_LOADER',payload:false})
+//     }
+//   }
+
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getProducts = createAsyncThunk(
+  'products/getProducts',
+  async(_, thunkAPI)=>{
+    const { dispatch, getState } = thunkAPI
+    dispatch({type:'SET_LOADER',payload:true})
+    const response = await fetch('https://fakestoreapi.com/products')
+    const data = await response.json()
+    dispatch({type:'SET_LOADER',payload:false})
+    return data
+
   }
+)
--- src/counter/index.jsx
+++ src/counter/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { counterSlice } from '../Reducer/counterReducer'
 
 const Counter = () => {
 const dispatch = useDispatch()
 const count = useSelector((state)=>state.count)
 const handleCounter = () => {
     // dispatch({type:'INCREMENT',payload:1})
+    // dispatch(counterSlice.actions.increment())
     dispatch({type:'counter/increment'})
+
 }
   return (
     <div>
